f3eca93773d733d2079b64cb08e859be2940be1b
NgHMHdev <115964188+NgHMHdev@users.noreply.github.com>
2024-02-23 21:18:16 +0700

    Update order.php

--- order.php
+++ order.php
@@ -4,40 +4,16 @@ include_once("ketnoi.php");
 class modelOrder
 {
     
-     function SelectThanhToanByIdTaiKhoanAndIdDonHang($idTaiKhoan, $idThanhToan)
-    {
-        $p = new clsketnoi();
-        $connect = $p->ketnoiDB();
-        $tbl = "SELECT * FROM `thanhtoan` INNER JOIN chitietthanhtoan ON thanhtoan.idThanhToan = chitietthanhtoan.idThanhToan
-         INNER JOIN taikhoan ON thanhtoan.idTaiKhoan = taikhoan.idTaiKhoan
-         WHERE thanhtoan.idTaiKhoan =$idTaiKhoan AND thanhtoan.idThanhToan = $idThanhToan";
-        $table = mysqli_query($connect, $tbl);
-        $list_users = array();
-        if (mysqli_num_rows($table) > 0) {
-            while ($row = mysqli_fetch_assoc($table)) {
-                $list_users[] = $row;
-            }
-            return $list_users;
-        }
-        $p->dongketnoi($connect);
-    }
-
-    function SelectThanhToanByIdTaiKhoan($idTaiKhoan)
-    {
-        $p = new clsketnoi();
-        $connect = $p->ketnoiDB();
-        $tbl = "SELECT * FROM `thanhtoan` INNER JOIN chitietthanhtoan ON thanhtoan.idThanhToan = chitietthanhtoan.idThanhToan
-         INNER JOIN taikhoan ON thanhtoan.idTaiKhoan = taikhoan.idTaiKhoan
-         WHERE thanhtoan.idTaiKhoan =$idTaiKhoan";
-        $table = mysqli_query($connect, $tbl);
-        $list_users = array();
-        if (mysqli_num_rows($table) > 0) {
-            while ($row = mysqli_fetch_assoc($table)) {
-                $list_users[] = $row;
-            }
-            return $list_users;
-        }
-        $p->dongketnoi($connect);
-    }
+  function h2(){
+    return '2';
+   }
+    
+   function h1(){
+    return '1';
+   }
+   
+   function h3(){
+    return '3';
+   }
        
 }
