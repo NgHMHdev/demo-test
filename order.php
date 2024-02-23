c278b0f8978937c7e87d3cce47eee33a0383e141
NGUYEN HO MINH HUAN <m.huan190102@gmail.com>
2024-02-23 20:37:15 +0700

    update order

--- order.php
+++ order.php
@@ -40,24 +40,7 @@ class modelOrder
     }
 
 
-    function SelectOrderByNgayLenMon($ngayLenMon)
-    {
-        $p = new clsketnoi();
-        $connect = $p->ketnoiDB();
-        $tbl = "SELECT * FROM taikhoan 
-        JOIN  dondatmon ON taikhoan.idTaiKhoan =  dondatmon.idTaiKhoan        
-        JOIN chitietdon ON dondatmon.idDon = chitietdon.idDon 
-        JOIN monan ON chitietdon.idMonAn = monan.idMonAn WHERE chitietdon.ngayLenMon = '$ngayLenMon'";
-        $table = mysqli_query($connect, $tbl);
-        $list_users = array();
-        if (mysqli_num_rows($table) > 0) {
-            while ($row = mysqli_fetch_assoc($table)) {
-                $list_users[] = $row;
-            }
-            return $list_users;
-        }
-        $p->dongketnoi($connect);
-    }
+   
 
 
     function SelectSumOrderPayByidTaiKhoan($idTaiKhoan)
